95bb49bc051f25fdabd918adf26d71b669718abd
Peter W <31324861+peterwwillis@users.noreply.github.com>
2023-11-05 15:03:23 -0500

    See if we can just skip the type

--- tests/local-file-hello-world.tfd/local-file.tf
+++ tests/local-file-hello-world.tfd/local-file.tf
@@ -1,12 +1,10 @@
 variable "file-name" {
   description = "A file to create in the local directory"
-  type = "string"
   default = "foo.bar.txt"
 }
 
 variable "insert-value" {
   description = "Insert a value into a file"
-  type = "string"
   default = "default"
 }
 
