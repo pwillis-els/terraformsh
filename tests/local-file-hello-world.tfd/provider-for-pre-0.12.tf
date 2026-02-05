0982e8ad1d880bde39f9ca3d0884bff93b991754
Th0masL <25387544+Th0masL@users.noreply.github.com>
2026-02-05 09:42:47 +0200

    Fix typo

--- tests/local-file-hello-world.tfd/provider-for-pre-0.12.tf
+++ tests/local-file-hello-world.tfd/provider-for-pre-0.12.tf
@@ -1,4 +1,4 @@
-# File to use to pin null_provider when Terraform version < 0.12
+# File to force local_provider version when Terraform version < 0.12
 # Note: This file is automatically deleted if Terraform >= 0.12
 provider "local" {
   version = "1.4.0"
--- tests/null-resource-hello-world.tfd/provider-for-pre-0.12.tf
+++ tests/null-resource-hello-world.tfd/provider-for-pre-0.12.tf
@@ -1,4 +1,4 @@
-# File to use to pin null_provider when Terraform version < 0.12
+# File to force null_provider version when Terraform version < 0.12
 # Note: This file is automatically deleted if Terraform >= 0.12
 provider "null" {
   version = "2.1.2"
