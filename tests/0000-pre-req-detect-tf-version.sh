d20f7f24530db69abb7dbf7c78860033ac497e2e
Th0masL <25387544+Th0masL@users.noreply.github.com>
2026-02-12 12:15:21 +0200

    Add CI tests for OpenTofu

--- tests/0000-pre-req-detect-tf-version.sh
+++ tests/0000-pre-req-detect-tf-version.sh
@@ -3,16 +3,31 @@
 [ "${DEBUG:-0}" = "1" ] && set -x
 set -u
 
+if [ -n "$( which terraform 2>/dev/null )" ]; then
+    TERRAFORM_SHORT_NAME="terraform"
+elif [ -n "$( which tofu 2>/dev/null )" ]; then
+    TERRAFORM_SHORT_NAME="tofu"
+else
+    echo "Error: Unable to find terraform or tofu."
+    exit 1
+fi
+
 # Detect terraform version (major + minor)
-TF_VER_FULL=$( terraform --version | grep '^Terraform v' | cut -d 'v' -f 2 )
+TF_VER_OUTPUT=$( $TERRAFORM_SHORT_NAME --version | grep -E '^Terraform v|^OpenTofu v' | head -1 )
+TERRAFORM_NICE_NAME=$( echo "$TF_VER_OUTPUT" | awk '{print $1}' ) 
+TF_VER_FULL=$( echo "$TF_VER_OUTPUT" | awk '{print $2}' | tr -d 'v' )
 TF_VER_MAJOR=$( echo "$TF_VER_FULL" | awk -F '.' '{print $1}' )
 TF_VER_MINOR=$( echo "$TF_VER_FULL" | awk -F '.' '{print $2}' )
 
 # For Terraform < 0.12, the Terraform plugin declaration is different
-if [ $TF_VER_MAJOR -eq 0 ] && [ $TF_VER_MINOR -lt 12 ]; then
-    TF_VER="pre-0.12"
+if [ "$TERRAFORM_SHORT_NAME" = "terraform" ]; then
+    if [ $TF_VER_MAJOR -eq 0 ] && [ $TF_VER_MINOR -lt 12 ]; then
+        TF_VER="pre-0.12"
+    else
+        TF_VER="post-0.12"
+    fi
 else
-    TF_VER="post-0.12"
+    TF_VER="$TF_VER_MAJOR.$TF_VER_MINOR"
 fi
 
-echo "Terraform Version: $TF_VER_FULL ($TF_VER)"
+echo "$TERRAFORM_NICE_NAME Version: $TF_VER_FULL ($TF_VER)"
